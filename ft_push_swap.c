1aece3a60a4449be95a215c5925eaabdac30dacc
aloysius-tim <tim@keynes.fr>
2018-02-17 15:39:22 +0100

    Working state but still not good enough shots

--- ft_push_swap.c
+++ ft_push_swap.c
@@ -9,13 +9,9 @@ int     main(int argc, char *argv[])
     int     nb_elems;
     int     nbOp;
     int     slide_size;
-    int     count;
-    int     nb_rotation;
-    int     nb_slides;
+    //int     nb_slides;
     int     to_remove;
 
-    nb_rotation = 1;
-    count = 0;
     nbOp = 0;
     nb_elems = 0;
     if (argc == 1)
@@ -29,48 +25,33 @@ int     main(int argc, char *argv[])
         nb_elems++;
     }
 
-    if (nb_elems <= 5)
-        nb_slides = 2;//15
-    else if (nb_elems <= 10)
-        nb_slides = 4;//35
-    else if (nb_elems <= 25)
-        nb_slides = 8;//115
-    else if (nb_elems <= 50)
-        nb_slides = 8;//300
-    else if (nb_elems <= 100)
-        nb_slides = 16;
-    else if (nb_elems <= 250)
-        nb_slides = 32;
-    else
-        nb_slides = 64;
-
-    slide_size = ft_ceil(nb_elems / nb_slides + 0 * (nb_elems / nb_slides)) + 1;
-    //print_stacks(a_stack, b_stack, "Before refractor");
     nb_elems = ft_refractor(a_stack);
-    //print_stacks(a_stack, b_stack, "After refractor");
 
-    while (ft_list_size(a_stack) != 0)
-    {
-        nbOp += ft_push_a_stack_slide(&a_stack, &b_stack, nb_elems, slide_size * (count + 1), nb_elems);
-        //print_stacks(a_stack, b_stack, "Iteration");
-        count++;
-    }
-    to_remove = nbOp;
+    slide_size = nb_elems / (nb_elems / 10);
+    to_remove = ft_push_slides(&a_stack, &b_stack, slide_size, slide_size, nb_elems);
 
-    //printf("\nNb Operations = %d\n", nbOp);
-    //printf("\n\nTime for fucking operations\n\n");
+    nbOp += ft_order_stack(a_stack, b_stack);
+
+    //print_stacks(a_stack, b_stack, "LAST print -- Should be ordered");
+    //printf("\nPre-operations = %d, Total Operations = %d", to_remove, nbOp);
+    return (1);
+}
+
+int     ft_order_stack(t_list  *a_stack, t_list *b_stack)
+{
+    int nb_rotation;
+    int nbOp;
+
+    nbOp = 0;
+    nb_rotation = 1;
     nbOp += ft_op("pa", &a_stack, &b_stack, 1);
 
     while (ft_list_size(b_stack) != 0)
     {
-        //print_stacks(a_stack, b_stack, "BEFORE");
-        if (b_stack && a_stack && *((int *)b_stack->data) > *((int *)a_stack->data) && *((int *)b_stack->data) - *((int *)a_stack->data) != 1)
+        if (b_stack && a_stack && ((*((int *)b_stack->data) - *((int *)a_stack->data) != 1) || *((int*)ft_list_last(a_stack)->data) - *((int *)b_stack->data) != 1))
         {
-
-            //Insert by the top
             if (*((int*)ft_list_last(a_stack)->data) - *((int *)b_stack->data) >= *((int *)b_stack->data) - *((int*)a_stack->data))
-            {
-                //printf("TOP\n");
+            {//Insert by the top
                 while (*((int *)a_stack->data) < *((int *)b_stack->data))
                 {
                     nb_rotation++;
@@ -78,8 +59,9 @@ int     main(int argc, char *argv[])
                 }
                 nbOp += ft_op("pa", &a_stack, &b_stack, 1);
                 nbOp += ft_op("rra", &a_stack, &b_stack, nb_rotation - 1);
-            }else{
-                //printf("END\n");
+            }
+            else
+            {//Insert by the bottom
                 while (*((int *)ft_list_last(a_stack)->data) > *((int *)b_stack->data))
                 {
                     nb_rotation++;
@@ -88,68 +70,51 @@ int     main(int argc, char *argv[])
                 nbOp += ft_op("pa", &a_stack, &b_stack, 1);
                 nbOp += ft_op("ra", &a_stack, &b_stack, nb_rotation);
             }
-
-
-            /*while (*((int *)ft_list_last(a_stack)->data) > *((int *)b_stack->data))
-            {
-                nb_rotation++;
-                nbOp += ft_op("rra", &a_stack, &b_stack, 1);
-            }
-            nbOp += ft_op("pa", &a_stack, &b_stack, 1);
-            nbOp += ft_op("ra", &a_stack, &b_stack, nb_rotation);*/
         }
-        if (b_stack && a_stack && *((int *)b_stack->data) > *((int *)a_stack->data) && *((int *)b_stack->data) - *((int *)a_stack->data) == 1)
+        else if (b_stack && a_stack && *((int *)b_stack->data) > *((int *)a_stack->data))
         {
             nbOp += ft_op("pa", &a_stack, &b_stack, 1);
             nbOp += ft_op("sa", &a_stack, &b_stack, 1);
         }
-        if (b_stack && a_stack && *((int *)b_stack->data) < *((int *)a_stack->data))
-        {
+        else if (b_stack && a_stack && *((int *)b_stack->data) < *((int *)a_stack->data))
             nbOp += ft_op("pa", &a_stack, &b_stack, 1);
-        }
         nb_rotation = 1;
         //print_stacks(a_stack, b_stack, "AFTER");
         //printf("\nPre-operations = %d, Total Operations = %d", to_remove, nbOp);
     }
-
-    //print_stacks(a_stack, b_stack, "LAST print -- Should be ordered");
-
-    //printf("\nPre-operations = %d, Total Operations = %d", to_remove, nbOp);
-    return (1);
+    return (nbOp);
 }
 
-int     ft_push_a_stack_slide(t_list **a_stack, t_list **b_stack, int nb_elems, int slide_size, int list_size)
+int     ft_push_slides(t_list **a_stack, t_list **b_stack, int max, int slide_size, int nb_elems)
 {
     int     counter;
-    int     current_size;
-    int     nbOp;
+    int     stack_size;
 
-    nbOp = 0;
     counter = 0;
-    current_size = ft_list_size(*a_stack);
-    (void)list_size;
-    //printf("\n%d - %d\n", slide_size, nb_elems);
-    while(counter < current_size)
+    stack_size = ft_list_size(*a_stack);
+    if (stack_size == 0)
+        return (counter);
+    while (counter++ < stack_size)
     {
-        if (*((int *)((*a_stack)->data)) <= slide_size || *((int *)((*a_stack)->data)) > nb_elems - slide_size) {
+        if (*a_stack && (*((int *)((*a_stack)->data)) <= max || *((int *)((*a_stack)->data)) >= nb_elems - max))
+        {
             counter += ft_op("pb", a_stack, b_stack, 1);
 
             if (ft_list_size(*b_stack) >= 2 &&
-                    *((int *)((*b_stack)->data)) < list_size / 2 &&
-                    *((int *)((*b_stack)->next->data)) < list_size / 2 &&
-                    *((int *)((*b_stack)->data)) < (*((int *)((*b_stack)->next->data))))
+                *((int *)((*b_stack)->data)) < nb_elems / 2 &&
+                *((int *)((*b_stack)->next->data)) < nb_elems / 2 &&
+                *((int *)((*b_stack)->data)) < (*((int *)((*b_stack)->next->data))))
                 counter += ft_op("sb", a_stack, b_stack, 1);
             else if (ft_list_size(*b_stack) >= 2 &&
-                *((int *)((*b_stack)->data)) > list_size / 2 &&
-                *((int *)((*b_stack)->next->data)) > list_size / 2 &&
-                *((int *)((*b_stack)->data)) > (*((int *)((*b_stack)->next->data))))
+                     *((int *)((*b_stack)->data)) > nb_elems / 2 &&
+                     *((int *)((*b_stack)->next->data)) > nb_elems / 2 &&
+                     *((int *)((*b_stack)->data)) > (*((int *)((*b_stack)->next->data))))
                 counter += ft_op("sb", a_stack, b_stack, 1);
         }
-        else counter += ft_op("ra", a_stack, b_stack, 1);
-        nbOp++;
-        //print_stacks(*a_stack, *b_stack, "Iteration");
+        else
+            counter += ft_op("ra", a_stack, b_stack, 1);
     }
-    return (nbOp);
+    return (counter + ft_push_slides(a_stack, b_stack, max + slide_size, slide_size, nb_elems));
 }
 
 int    ft_refractor(t_list *stack)
